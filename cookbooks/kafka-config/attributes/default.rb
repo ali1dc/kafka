ecdba67cbaee6624a202d0371101798307b504b2
Ali Jafari <ali.jafari@excella.com>
2018-05-12 12:39:45 -0400

    use plaintext protocol

--- cookbooks/kafka-config/attributes/default.rb
+++ cookbooks/kafka-config/attributes/default.rb
@@ -9,8 +9,8 @@ default['confluent']['scala_version'] = '2.11'
 
 default['confluent']['kafka']['server.properties'].tap do |broker|
   broker['broker.id'] = 0
-  broker['listeners'] = 'SASL_SSL://IP:9092'
-  broker['advertised.listeners'] = 'SASL_SSL://IP:9092'
+  broker['listeners'] = 'PLAINTEXT://IP:9092'
+  broker['advertised.listeners'] = 'PLAINTEXT://IP:9092'
   broker['zookeeper.connect'] = '10.100.1.100:2181,10.100.2.100:2181,10.100.3.100:2181'
   broker['log.dirs'] = '/kafkalogs/logs'
 end
