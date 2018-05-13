52dd3d288b65fd2bcddd84169647a623b8d63e75
Ali Jafari <ali.jafari@excella.com>
2018-05-13 17:36:39 -0400

    cleanup

--- cookbooks/kafka-config/spec/unit/recipes/default_spec.rb
+++ cookbooks/kafka-config/spec/unit/recipes/default_spec.rb
@@ -4,7 +4,7 @@
 # Cookbook:: kafka-config
 # Spec:: default
 #
-# Copyright:: 2018, The Authors, All Rights Reserved.
+# Copyright:: 2018, Ali Jafari - Excella Data Lab, All Rights Reserved.
 
 require 'spec_helper'
 
@@ -14,22 +14,6 @@ describe 'kafka-config::default' do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
       runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(
-        platform: 'centos',
-        version: '7.4.1708'
-      )
       runner.converge(described_recipe)
     end
 
